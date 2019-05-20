db813fa626c1fc67df4f48fae3dc447e4b2dab6d
yahir <yahirruizmen0@gmail.com>
2019-05-20 13:59:09 -0500

    8

---
+++ js/acciones.js
@@ -0,0 +1,32 @@
+//JavaScript
+
+$(document).ready(function(e) {
+    document.addEventListener("deviceready",onDeviceReady,false);
+	
+});
+
+function onDeviceReady(){
+	
+	$('#position').on ('click', function (){
+		getPosition ();
+	});
+}
+
+function getPosition(){
+	var options = {
+		enableHighAccuracy : true,
+		maximunAge: 3600000
+	}
+	
+	var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+	
+	function onSuccess(position){
+		
+		alert('Latitude:'  +position.coords.latitude  +'\n'+
+		'Longitude:'       +position.coords.latitude  +'\n');
+	};
+	
+	function onError(error){
+		alert('code:' +error.code +'\n'+'message:'+error.message+'\n');
+	}
+}
